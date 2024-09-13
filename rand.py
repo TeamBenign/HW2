049e5d53fd72037c499baa2f621c0b0f6b0b5809
nazmul-md <bsse0635@iit.du.ac.bd>
2024-09-13 04:07:58 +0000

    Push from automated tools

--- rand.py
+++ rand.py
@@ -1,6 +1,6 @@
 """random module to generate random numbers"""
 
-import subprocess # nosec
+import subprocess  # nosec
 
 
 def random_array(arr):
@@ -8,6 +8,6 @@ def random_array(arr):
     for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
             ["/usr/bin/shuf", "-i1-20", "-n1"],
-            capture_output=True, check=True)# nosec
+            capture_output=True, check=True)  # nosec
         arr[i] = int(shuffled_num.stdout)
     return arr
